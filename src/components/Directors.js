0dfcb6bfaa0009cb9384887d19e97e0f02321262
ombasaabuya <ombasabuya@gmail.com>
2023-01-27 08:04:12 +0300

    added Directors component

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { directors } from "../data";
-
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayDirectors = directors.map((director)=>{
+    return(<div key={director.name}>
+      <h2>{`Name : ${director.name}`}</h2>
+      <h2>Movies:</h2>
+      <ul>
+        {
+          director.movies.map((movie) =>
+          {return(
+            <li key={movie}>{movie}</li>
+          )
+              
+          })
+        }
+      </ul>
+
+    </div>)})
+  return <div>{/*{code here}*/}
+  <h1>Directors Page</h1>
+  {displayDirectors}
+  </div>;
 }
 
 export default Directors;
