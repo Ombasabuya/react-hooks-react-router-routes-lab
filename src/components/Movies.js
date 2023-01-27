6afc470bf15c7e30d7eae3a698ab44cad74316be
ombasaabuya <ombasabuya@gmail.com>
2023-01-27 08:05:44 +0300

    modified Movie component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { generatePath } from "react-router-dom";
 import { movies } from "../data";
-
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const displayMoviedata = movies.map((movie)=>
+  {return (
+    <div key={movie.title}>
+        <h2>{`Name :${movie.title}`}</h2>
+        <p>{`Time: ${movie.time}`}</p>
+          <h2>Genres</h2>
+          <ul>
+            {movie.genres.map((genre) =>
+            {return(
+              <li key={genre}>{genre}</li>
+            )     
+            })
+          }
+
+          </ul>
+    </div>    
+  )
+    
+  })
+  return <div>
+     <h1>Movies Page</h1>
+    {displayMoviedata}
+  </div>;
 }
 
 export default Movies;
