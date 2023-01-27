6740bda7fa557ee6506f7aa5c366a35732952fcb
ombasaabuya <ombasabuya@gmail.com>
2023-01-27 08:02:01 +0300

    Actors component added

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { actors } from "../data";
-
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayActors = actors.map((actor) => {
+    return (
+      <div key={actor.name}>
+        <h2>{`Name: ${actor.name}`}</h2>
+          <h2>Movies:</h2>
+          <ul>
+            {actor.movies.map((element)=>
+            {return(
+              <li key={element}>{element}</li>
+   ) })}
+          </ul>
+      </div>
+    )
+ }
+  )
+  return (<div>
+  <h1>Actors Page</h1>
+  {displayActors}
+  </div>);
 }
 
 export default Actors;
